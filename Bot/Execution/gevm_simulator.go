1e08978dac9878e076b88402a521821c567b8021
Creation123a <atharvaannes18@gmail.com>
2026-08-10 16:13:02 +0530

    Update gevm_simulator.go

--- Bot/Execution/gevm_simulator.go
+++ Bot/Execution/gevm_simulator.go
@@ -16,7 +16,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 
-	"my-mev-bot/Types"
+	"my-mev-bot/Bot/Types"
 )
 
 // SimBackend defines the simulation backend type.
