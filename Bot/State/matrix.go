5a160ad982db9de93dda0a871b1f19d90714172a
Creation123a <atharvaannes18@gmail.com>
2026-08-10 16:15:30 +0530

    Update matrix.go

--- Bot/State/matrix.go
+++ Bot/State/matrix.go
@@ -17,8 +17,8 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 
-	"my-mev-bot/config"
-	"my-mev-bot/types"
+	"my-mev-bot/Bot/Config"
+	"my-mev-bot/Bot/Types"
 )
 
 const (
