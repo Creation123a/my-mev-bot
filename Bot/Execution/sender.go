6b1a0e88eac053ea60f886e68776d2aca755e240
Creation123a <atharvaannes18@gmail.com>
2026-08-10 16:13:43 +0530

    Update sender.go

--- Bot/Execution/sender.go
+++ Bot/Execution/sender.go
@@ -19,9 +19,9 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 
-	"my-mev-bot/Config"
-	"my-mev-bot/Solver"
-	"my-mev-bot/Types"
+	"my-mev-bot/Bot/Config"
+	"my-mev-bot/Bot/Solver"
+	"my-mev-bot/Bot/Types"
 )
 
 type PendingTx struct {
