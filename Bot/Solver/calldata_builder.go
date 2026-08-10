e0ba68ae4248a89857cfde2e3810bc058d83a8f4
Creation123a <atharvaannes18@gmail.com>
2026-08-10 16:10:23 +0530

    Update calldata_builder.go

--- Bot/Solver/calldata_builder.go
+++ Bot/Solver/calldata_builder.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
-	"my-mev-bot/types"
+	"my-mev-bot/Types"
 )
 
 // NOTE: This ABI definition must exactly match the Solidity contract's
